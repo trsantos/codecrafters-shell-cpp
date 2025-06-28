32829d21ffbace1fdb1d2c93a871491aec177f58
Thiago Rodrigues Santos <thiago@trsantos.com>
2025-06-27 22:15:09 -0300

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
+    while (true) {
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+        if (std::cin.good())
+            std::cout << input << ": command not found" << std::endl;
+        else
+            break;
+    }
 }
